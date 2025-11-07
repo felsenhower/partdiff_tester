805360212ec9974b58899278e77256903925a1e4
Ruben Felgenhauer <Ruben.Felgenhauer@posteo.de>
2025-11-07 12:11:58 +0100

    Bugfix Handle timeout correctly and check for missing argument

--- make_reference.sh
+++ make_reference.sh
@@ -1,13 +1,37 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 
 set -euo pipefail
+IFS=$'\n\t'
+
+[ "${SHELLCHECK:-0}" == "1" ] && shellcheck "$0"
+
+function usage {
+cat << EOF
+Usage: ./make_reference.sh COMMAND [ARG]...
+
+Create partdiff reference data.
+
+COMMAND is the path to the partdiff executable.
+
+Example:
+    ./make_reference.sh /path/to/partdiff
+EOF
+}
+
+if (( "$#" == 0 )) ; then
+    usage
+    exit 1
+fi
 
 PARTDIFF_EXEC=( "$@" )
 
-param_num_range=(1)
-param_method_range=(1 2)
-param_lines_range=(0 1 10 100 1000)
-param_func_range=(1 2)
+TIMEOUT_DURATION=2
+OUTPUT_DIRECTORY='./reference_data'
+
+PARAM_NUM_RANGE=(1)
+PARAM_METHOD_RANGE=(1 2)
+PARAM_LINES_RANGE=(0 1 10 100 1000)
+PARAM_FUNC_RANGE=(1 2)
 
 function run_partdiff {
     num="$1"
@@ -16,32 +40,38 @@ function run_partdiff {
     func="$4"
     term="$5"
     preciter="$6"
-    echo ${PARTDIFF_EXEC[@]} "$num" "$method" "$lines" "$func" "$term" "$preciter"
+    echo -n "${PARTDIFF_EXEC[@]}" "$num" "$method" "$lines" "$func" "$term" "$preciter"
     set +e
-    output="$(timeout 2 ${PARTDIFF_EXEC[@]} "$num" "$method" "$lines" "$func" "$term" "$preciter")"
+    output="$(timeout "$TIMEOUT_DURATION" "${PARTDIFF_EXEC[@]}" "$num" "$method" "$lines" "$func" "$term" "$preciter")"
     exit_code="$?"
     set -e
     success="$((exit_code == 0))"
     if (( success )) ; then
-        output_filename="$(printf 'partdiff_%s_%s_%s_%s_%s_%s.txt\n' "$num" "$method" "$lines" "$func" "$term" "$preciter")"
+        output_filename="$(printf '%s/partdiff_%s_%s_%s_%s_%s_%s.txt\n' "$OUTPUT_DIRECTORY" "$num" "$method" "$lines" "$func" "$term" "$preciter")"
         echo "$output" > "$output_filename"
+        echo '  (OK)'
+        return
     fi
-    timed_out="$((exit_code == 125))"
-    if ! (( timed_out )) ; then
-        echo "$output"
-        exit "$exit_code"
+    timed_out="$((exit_code == 124))"
+    if (( timed_out )) ; then
+        echo '  (TIMED OUT)'
+        return
     fi
+    echo "$output"
+    exit "$exit_code"
 }
+
+mkdir -p "$OUTPUT_DIRECTORY"
 
 (
     param_term_range=(2)
     param_preciter_range=(1 10 100 1000 10000 100000)
-    for num in ${param_num_range[@]} ; do
-        for method in ${param_method_range[@]} ; do
-            for lines in ${param_lines_range[@]} ; do
-                for func in ${param_func_range[@]} ; do
-                    for term in ${param_term_range[@]} ; do
-                        for preciter in ${param_preciter_range[@]} ; do
+    for num in "${PARAM_NUM_RANGE[@]}" ; do
+        for method in "${PARAM_METHOD_RANGE[@]}" ; do
+            for lines in "${PARAM_LINES_RANGE[@]}" ; do
+                for func in "${PARAM_FUNC_RANGE[@]}" ; do
+                    for term in "${param_term_range[@]}" ; do
+                        for preciter in "${param_preciter_range[@]}" ; do
                             run_partdiff "$num" "$method" "$lines" "$func" "$term" "$preciter"
                         done
                     done
@@ -54,12 +84,12 @@ function run_partdiff {
 (
     param_term_range=(1)
     param_preciter_range=('1e-4' '1e-8' '1e-12' '1e-16' '1e-20')
-    for num in ${param_num_range[@]} ; do
-        for method in ${param_method_range[@]} ; do
-            for lines in ${param_lines_range[@]} ; do
-                for func in ${param_func_range[@]} ; do
-                    for term in ${param_term_range[@]} ; do
-                        for preciter in ${param_preciter_range[@]} ; do
+    for num in "${PARAM_NUM_RANGE[@]}" ; do
+        for method in "${PARAM_METHOD_RANGE[@]}" ; do
+            for lines in "${PARAM_LINES_RANGE[@]}" ; do
+                for func in "${PARAM_FUNC_RANGE[@]}" ; do
+                    for term in "${param_term_range[@]}" ; do
+                        for preciter in "${param_preciter_range[@]}" ; do
                             run_partdiff "$num" "$method" "$lines" "$func" "$term" "$preciter"
                         done
                     done
